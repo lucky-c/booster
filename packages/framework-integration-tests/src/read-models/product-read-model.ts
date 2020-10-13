cfb1318446355d905afa1981caf1cce86c3d5722
Nikita Tchayka <nick@booster.cloud>
2020-10-13 12:30:33 +0100

    WIP: Adding tests

--- packages/framework-integration-tests/src/read-models/product-read-model.ts
+++ packages/framework-integration-tests/src/read-models/product-read-model.ts
@@ -1,5 +1,5 @@
 import { ReadModel, Projects } from '@boostercloud/framework-core'
-import { UserWithEmail } from '../roles'
+// import { UserWithEmail } from '../roles'
 import { UUID } from '@boostercloud/framework-types'
 import { Product } from '../entities/Product'
 import { SKU } from '../common/sku'
@@ -7,7 +7,7 @@ import { Money } from '../common/money'
 
 // This is an example read model for a possible admin-exclusive report to show last and previous updates to products
 @ReadModel({
-  authorize: [UserWithEmail],
+  authorize: 'all',
 })
 export class ProductReadModel {
   public constructor(
